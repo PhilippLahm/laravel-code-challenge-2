656066bcbeb25fcea6439fedcad675ff0151aeea
Hà Ngọc Lâm <hangoclam250595@gmail.com>
2021-07-21 22:22:49 +0700

    Exercise #2

--- database/factories/ScheduledRepaymentFactory.php
+++ database/factories/ScheduledRepaymentFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Loan;
 use App\Models\ScheduledRepayment;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -22,7 +23,14 @@ class ScheduledRepaymentFactory extends Factory
     public function definition(): array
     {
         return [
-            // TODO: Complete factory
+            'loan_id' => fn() => Loan::factory()->create(),
+            'amount' => $this->faker->randomNumber(),
+            'outstanding_amount' => $this->faker->randomNumber(),
+            'currency_code' => $this->faker->randomElement([
+                Loan::CURRENCY_VND, Loan::CURRENCY_SGD
+            ]),
+            'due_date' => $this->faker->dateTimeBetween('+3 month', '+6 month'),
+            'status' => ScheduledRepayment::STATUS_DUE,
         ];
     }
 }
--- database/migrations/2020_01_28_100001_create_scheduled_repayments_table.php
+++ database/migrations/2020_01_28_100001_create_scheduled_repayments_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use \App\Models\ScheduledRepayment;
 
 class CreateScheduledRepaymentsTable extends Migration
 {
@@ -16,8 +17,11 @@ class CreateScheduledRepaymentsTable extends Migration
         Schema::create('scheduled_repayments', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('loan_id');
-
-            // TODO: Add missing columns here
+            $table->integer('amount');
+            $table->integer('outstanding_amount')->default(0);
+            $table->string('currency_code');
+            $table->date('due_date');
+            $table->string('status')->default(ScheduledRepayment::STATUS_DUE);
 
             $table->timestamps();
             $table->softDeletes();
